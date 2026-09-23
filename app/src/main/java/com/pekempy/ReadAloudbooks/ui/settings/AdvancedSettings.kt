056678b5fc10696e289e6938fe053e2529281415
pekempy <pete@pekempy.co.uk>
2026-09-23 10:04:59 +0100

    Use qualified Color.Transparent - build clean

--- app/src/main/java/com/pekempy/ReadAloudbooks/ui/settings/AdvancedSettings.kt
+++ app/src/main/java/com/pekempy/ReadAloudbooks/ui/settings/AdvancedSettings.kt
@@ -192,8 +192,8 @@ fun AdvancedSettingsScreen(
                     .padding(16.dp),
                 shape = RoundedCornerShape(12.dp),
                 colors = TextFieldDefaults.colors(
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent
+                    focusedIndicatorColor = androidx.compose.ui.graphics.Color.Transparent,
+                    unfocusedIndicatorColor = androidx.compose.ui.graphics.Color.Transparent
                 )
             )
             
