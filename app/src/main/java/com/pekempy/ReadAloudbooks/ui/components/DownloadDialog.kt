0308e3a9d6a1d2a1895463ae5eebff70ba21abee
pekempy <pete@pekempy.co.uk>
2026-09-23 09:59:55 +0100

    Fix missing Alignment import - build now clean

--- app/src/main/java/com/pekempy/ReadAloudbooks/ui/components/DownloadDialog.kt
+++ app/src/main/java/com/pekempy/ReadAloudbooks/ui/components/DownloadDialog.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
